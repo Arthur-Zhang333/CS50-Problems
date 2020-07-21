802e3ca75aa32c1ae1151d7ed2fe1cdd2293e91e
Arthur-Zhang333 <66291890+Arthur-Zhang333@users.noreply.github.com>
2020-07-21 13:07:48 -0700

    Update Movies.sql

--- Movies.sql
+++ Movies.sql
@@ -1,3 +1,10 @@
+#Specification
+#For each of the following problems, you should write a single SQL query that outputs the results specified by each problem. 
+#Your response must take the form of a single SQL query, though you may nest other queries inside of your query. 
+#You should not assume anything about the ids of any particular movies or people: your queries should be accurate even if the id of any particular movie or person were different. 
+#Finally, each query should return only the data necessary to answer the question: if the problem only asks you to output the names of movies, 
+#for example, then your query should not also output the each movie’s release year.
+
 #In 1.sql, write a SQL query to list the titles of all movies released in 2008.
 SELECT * FROM movies WHERE year = 2008;
 
